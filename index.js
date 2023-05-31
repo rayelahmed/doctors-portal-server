a58a1fb0ade6870c762cfde391f243bc29bc148b
rayel_ahmed <rayelahhmed404@gmail.com>
2023-05-31 08:21:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gu69ltc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      // Step 1: Get all services
+      const services = await serviceCollection.find().toArray();
+
+      // Step 2: Get the bookings for the specified date
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      res.send(bookings);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const { slot, date } = booking;
+
+      const existingBooking = await bookingCollection.findOne({ slot, date });
+      if (existingBooking) {
+        return res.send({
+          success: false,
+          message: `Already have an appointment on ${date} at ${slot}`,
+        });
+      }
+
+      const result = await bookingCollection.insertOne(booking);
+
+      return res.send({ success: true, result });
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
